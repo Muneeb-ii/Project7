29a3b6d2fcff554e5a96f1329302df47106bb7f0
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-23 06:35:29 -0400

    Add constructors

--- src/Heap.java
+++ src/Heap.java
@@ -4,8 +4,39 @@
  * Purpose of class: A generic implementation of a heap data structure that implements
  * the PriorityQueue interface. 
  */
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Heap<T> implements PriorityQueue<T>{
     private Comparator<T> comparator;
     private ArrayList<T> heap;
+
+    /**
+     * Constructor for the Heap class.
+     * 
+     * @param comparator the comparator to use for ordering the elements in the heap
+     * @param maxHeap    true if the heap should be a max heap, false if it should be a min heap
+     */
+    public Heap(Comparator<T> comparator, boolean maxHeap){
+        if(maxHeap){
+            this.comparator = new Comparator<T>(){
+                @Override
+                public int compare(T o1, T o2) {
+                    return ((Comparable<T>) o2).compareTo(o1);
+                }
+            };
+        }
+        else{
+            this.comparator = comparator;
+        }
+    }
+
+    /**
+     * Constructor for the Heap class.
+     * 
+     * @param comparator the comparator to use for ordering the elements in the heap
+     */
+    public Heap(Comparator<T> comparator){
+        this(comparator, false);
+    }
 }
