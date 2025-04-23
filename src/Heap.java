08fef5596c39447dea7cef8ed818d93d33ec8de5
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-04-23 06:26:07 -0400

    Create Heap class

---
+++ src/Heap.java
@@ -0,0 +1,10 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of class: A generic implementation of a heap data structure that implements
+ * the PriorityQueue interface. 
+ */
+
+public class Heap<T> implements PriorityQueue<T>{
+
+}
