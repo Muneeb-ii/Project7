bb1899453c419636a5b3f1500fc5a819b452ff20
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-05-01 14:59:48 -0400

    Create AbstractMazeClass and add multiple fields

---
+++ src/AbstractMazeClass.java
@@ -0,0 +1,30 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of class: An abstract class that is used to represent different maze searching
+ * algorithms. 
+ * 
+*/
+
+public class AbstractMazeClass {
+    
+    /**
+     * Maze object that is used to represent the maze
+     */
+    private Maze maze;
+
+    /**
+     * The starting cell of the maze
+     */
+    private Cell start;
+
+    /**
+     * The target cell of the maze
+     */
+    private Cell target;
+
+    /**
+     * The current cell being explored
+     */
+    private Cell cur;
+}
