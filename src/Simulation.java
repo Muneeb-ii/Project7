33aabbace3afaed94931d4a65f418df2fd9c5413
Muneeb Azfar Nafees <muneebazf05@gmail.com>
2025-05-01 22:16:45 -0400

    Create Simulation class and add analysisOne method

---
+++ src/Simulation.java
@@ -0,0 +1,35 @@
+/**
+ * Author: Muneeb Azfar Nafees
+ * 
+ * Purpose of the class: This class is used to run simulations for the maze searching algorithms. 
+ */
+
+public class Simulation {
+
+    /**
+     * Finds the relationship between density of obstacles and 
+     * probability of reaching the target - Analysis 1
+     */
+    public void analysisOne(){
+        double density = 0.0;
+
+        while (density <= 1.0){
+            int count = 0;
+            for(int i = 0; i < 10; i++){
+                Maze maze = new Maze(20,20, density);
+                MazeAStarSearch astar = new MazeAStarSearch(maze);
+                astar.search(maze.getStart(), maze.getTarget(), false, 0);
+                if(astar.traceback(astar.getTarget()) != null){
+                    count++;
+                }
+            }
+            System.out.println("Density: " + density + " - Paths Found: " + count + " out of 10");
+            System.out.println("Probability" + count/10.0);
+            density += 0.1;
+        }
+    }
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.analysisOne();
+    }
+}
